4f7597f70495297213e7abdf575fab1cb41e7afb
Alex Ozdemir <aozdemir@hmc.edu>
2015-01-15 09:39:25 -0800

    Switched to an approach which used the hough transform

--- beadfinder2.py
+++ beadfinder2.py
@@ -2,36 +2,32 @@
 import cv2
 import numpy as np
 
+def find_circles(img, small_r, big_r):
+    img = cv2.GaussianBlur(img, (9, 9), 0)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles(img,
+                               cv2.cv.CV_HOUGH_GRADIENT,
+                               3,
+                               2 * small_r,
+                               minRadius = small_r,
+                               maxRadius = big_r)
+    
+    return circles
+
+def draw_circles(output, circles):
+    for (x, y, r) in circles[0]:
+        cv2.circle(output, (x, y), r, (0, 0, 255), 2)
+
+
+
 def main():
-    img = cv2.imread("sample2.jpg")
-    #img = cv2.imread("template1.jpg")
-    #img = cv2.imread("ideal-one-circle.jpg")
-    img = cv2.GaussianBlur(img, (5, 5), 0)
-    img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_g = 255 - img_g
-    img_gray_sobel = cv2.Sobel(img_g, -1, 1, 1, ksize =3, scale = 20)
-    #cv2.imwrite("sobel.jpg",img_gray_sobel)
-    small_r = 60
-    big_r  = small_r + 5
-    pixels_wide = 2 * big_r + 1
-    kernel = np.zeros((pixels_wide, pixels_wide))
-    #print kernel.shape
-    center = (big_r + 1, big_r + 1);
-    count  = 0
-    for r in range(kernel.shape[0]):
-        for c in range(kernel.shape[1]):
-            radius_sq = (center[0] - r) ** 2 + (center[1] - c) ** 2
-            #print "[%d,%d] -> %d" % (r, c, radius_sq ** 0.5)
-            if (radius_sq > small_r ** 2 and radius_sq <= big_r ** 2):
-                #print 'match'
-                kernel[r][c] = 1
-                count += 1
-    #cv2.imwrite("img_invert.jpg", img_g)
-    #print 1. / count
-    kernel = np.multiply(kernel, 1. / count)
-    #print "Sum: %d" % (kernel.sum(),)
-    filtered = cv2.filter2D(img_gray_sobel, -1, kernel) # -1 -> use default
-    cv2.imwrite("output1.jpg", np.multiply(filtered, 1))
-    #cv2.imshow('filter1', filtered)
+    img = cv2.imread("sample1.jpg")
+    output = img.copy()
+
+    circles = find_circles(img, 40, 60)
+
+    draw_circles(output, circles)
+
+    cv2.imwrite('circles.jpg', output)
 
 main()
