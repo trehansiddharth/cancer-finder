98f3718d7dddbbb5c751ea38ff038d443fdf1d12
Alex Ozdemir <aozdemir@hmc.edu>
2015-01-15 15:16:51 -0800

    Kept wide search radius and used filling to reduce finding of interior circles

--- beadfinder2.py
+++ beadfinder2.py
@@ -1,19 +1,34 @@
 # BeadFinder 2
 import cv2
 import numpy as np
+import sys
 
-def find_circles(img, small_r, big_r):
+def find_circles(img, small_r, big_r, circles = None):
+
+    img = img.copy()
+
+    if circles != None:
+        print 'fill'
+        fill_circles(img, circles)
+
+    cv2.imwrite('filled.jpg', img)
+
     blur = odd((img.shape[0] + img.shape[1]) * 0.02)
     img = cv2.GaussianBlur(img, (blur, blur), 0)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('blur.jpg', img)
+
+    cv2.imwrite('blurred.jpg', img)
+
     thresh = img.mean()
+
     img = cv2.adaptiveThreshold(img,
                                 255,
                                 cv2.ADAPTIVE_THRESH_MEAN_C,
                                 cv2.THRESH_BINARY_INV,
                                 odd((img.shape[0] + img.shape[1]) * 0.05),
                                 thresh / 4)
+
+
 
     cv2.imwrite('thresh.jpg', img)
 
@@ -35,16 +50,30 @@ def draw_circles(output, circles):
     for (x, y, r) in circles[0]:
         cv2.circle(output, (x, y), r, (0, 0, 255), 2)
 
-
+def fill_circles(output, circles, color = (0,0,0)):
+    for (x, y, r) in circles[0]:
+        cv2.circle(output, (x, y), r, color, -1)
 
 def main():
-    img = cv2.imread("sample2.jpg")
-    output = img.copy()
+
+    filename = sys.argv[1]
+    print "Filename:", filename
+
+
+    img = cv2.imread(filename)
 
     circles = find_circles(img, 30, 80)
 
-    draw_circles(output, circles)
+    filled_img = img.copy()
+    fill_circles(filled_img, circles)
 
-    cv2.imwrite('circles.jpg', output)
+    cv2.imwrite('filled.jpg', filled_img)
+
+    circles = find_circles(img, 30, 80, circles)
+
+    out = img.copy()
+    draw_circles(out, circles)
+
+    cv2.imwrite('circles-%s.jpg' % (filename), out)
 
 main()
